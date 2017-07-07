bb60bda7752816da16d232ab9ab3e0d14ccc89a5
emanreyalp <emanreyalp@users.noreply.github.com>
2017-07-07 14:51:52 +0200

    read TADs from file and remove comments from HTML

--- url_reader.py
+++ url_reader.py
@@ -1,7 +1,15 @@
 import urllib2
+import re
 
-link = "http://portal.vik.bme.hu/kepzes/targyak/VIAUAC05/"
+def remove_comments(raw_html):
+    return re.sub(r"<!--.*-->", "", raw_html, flags=re.DOTALL)
+
+with open('TADs') as TADs_file:
+    TADs = TADs_file.readlines()
+
+link = "http://portal.vik.bme.hu/kepzes/targyak/%s/" % TADs[0]
 f = urllib2.urlopen(link)
 myfile = f.read()
+myfile2 = remove_comments(myfile)
 
-print(myfile)
+print(myfile2)
