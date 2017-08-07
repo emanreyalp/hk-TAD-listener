4653d0d2a9b4a98da556d3f0ef6ebe780a796e0a
emanreyalp <emanreyalp@sch.bme.hu>
2017-08-07 16:22:24 +0200

    added TAD class

---
+++ TAD.py
@@ -0,0 +1,66 @@
+class TAD_table(object):
+	"""docstring for TAD_table"""
+	def __init__(self, arg):
+		super(TAD_table, self).__init__()
+		self.arg = arg
+
+	def subject_code:
+
+	def semester:
+
+	def requirements:
+
+	def credit:
+
+	def subject_semester:
+
+class TAD(object):
+	"""docstring for TAD"""
+	def __init__(self, arg):
+		super(TAD, self).__init__()
+		self.arg = arg
+	
+	# TAD_table ! fontos
+	def infos_table:
+
+	# tantárgyfelelős személy és tanszék
+	def responsible_lecturer_and_department:
+
+	# témakörök amikre épít
+	def builded_topics:
+
+	# előtanulmányirend ! fontos
+	def final_requirement:
+
+	# célkitűzés
+	def objective:
+
+	# részletes tematika
+	def thematics:
+
+	# oktatás módja
+	def mode_of_education:
+
+	# követelmények ! important
+	def requirements:
+
+	# pótlási lehetőségek ! important
+	def replacement:
+
+	# konzultációs lehetőségek ! important
+	def consultations:
+
+	# jegyzet, tankönyv, felhasználható irodalom
+	def lecture_notes:
+
+	# elvégzéshez szükséges tanulmányi munka
+	def necessary_study_work:
+
+	# tematikát kidolgozta
+	def creator_of_thematics:
+
+	# IMSc tematika és módszertan
+	def thematics_of_imsc:
+	
+	# IMSc pontozás
+	def imsc_scoring:
